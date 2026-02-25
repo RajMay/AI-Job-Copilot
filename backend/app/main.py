a23c0fcca32d37c52d024e5a55d6a179151ae55a
Rajesh Burman <your@email.com>
2026-02-26 01:03:18 +0530

    change

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routes import resume
-from app.routes import query
+
 from app.routes import jobs
 from app.routes import ai_job
 import asyncio
@@ -17,7 +17,7 @@ app = FastAPI(title="AI Job Copilot API")
 
 app.include_router(resume.router)
 
-app.include_router(query.router)
+
 app.include_router(jobs.router)
 app.include_router(ai_job.router)
 
